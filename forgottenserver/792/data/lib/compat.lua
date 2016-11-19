91925a78b5af0f18efee7978f02479bd9ccac924
Tryller <tryllerot@gmail.com>
2016-11-19 11:00:57 -0200

    * Fix energy spells

--- forgottenserver/792/data/lib/compat.lua
+++ forgottenserver/792/data/lib/compat.lua
@@ -1,12 +1,3 @@
-TRUE = true
-FALSE = false
-
-LUA_ERROR = false
-LUA_NO_ERROR = true
-
-CONST_ME_PUFF = CONST_ME_POFF
-CONST_ME_ENERGYAREA = CONST_ME_TELEPORT
-
 getPlayerHealth = getCreatureHealth
 getPlayerMaxHealth = getCreatureMaxHealth
 getPlayerMana = getCreatureMana
