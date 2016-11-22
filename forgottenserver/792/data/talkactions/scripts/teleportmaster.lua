7a883000e8eca39fff0f07e8385660149f55d7be
Tryller <tryllerot@gmail.com>
2016-11-22 18:09:27 -0200

    Lua scipt using spaces

--- forgottenserver/792/data/talkactions/scripts/teleportmaster.lua
+++ forgottenserver/792/data/talkactions/scripts/teleportmaster.lua
@@ -2,25 +2,25 @@ function onSay(cid, words, param)
 	local tid = cid
 	local pos = getPlayerTown(tid)
 	local tmp = getTownName(pos)
-	if(not tmp) then
+	if (not tmp) then
 		doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Home town does not exists.")
 		return false
 	end
 
 	pos = getTownTemplePosition(pos)
-	if(not pos or isInArray({pos.x, pos.y}, 0)) then
+	if (not pos or isInArray({pos.x, pos.y}, 0)) then
 		doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Wrong temple position for town " .. tmp .. ".")
 		return false
 	end
 
 	pos = getClosestFreeTile(tid, pos)
-	if(not pos or isInArray({pos.x, pos.y}, 0)) then
+	if (not pos or isInArray({pos.x, pos.y}, 0)) then
 		doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Destination not reachable.")
 		return false
 	end
 
 	tmp = getCreaturePosition(tid)
-	if(doTeleportThing(tid, pos, true) and not isPlayerGhost(tid)) then
+	if (doTeleportThing(tid, pos, true) and not isPlayerGhost(tid)) then
 		doSendMagicEffect(tmp, CONST_ME_POFF)
 		doSendMagicEffect(pos, CONST_ME_TELEPORT)
 	end
