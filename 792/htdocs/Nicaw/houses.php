c410969a2e4c289f4e1df6fcd887af3b261a35fe
João Paulo <jprzimba@gmail.com>
2024-03-24 13:46:50 -0300

    * update houses

--- 792/htdocs/Nicaw/houses.php
+++ 792/htdocs/Nicaw/houses.php
@@ -17,36 +17,44 @@
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 include ("include.inc.php");
-$ptitle="Houses - $cfg[server_name]";
+$ptitle = "Houses - $cfg[server_name]";
 include ("header.inc.php");
-if (!is_file($cfg['dirdata'].$cfg['house_file']))
-	throw new aacException('House file not found: '.$cfg['dirdata'].$cfg['house_file']);
+if (!is_file($cfg['dirdata'] . $cfg['house_file']))
+    throw new aacException('House file not found: ' . $cfg['dirdata'] . $cfg['house_file']);
 ?>
 <div id="content">
-<div class="top">Houses</div>
-<div class="mid">
-<div style="height: 500px; overflow: auto; margin: 10px;">
-<table>
-<tr class="color0"><td width="35%"><b>House</b></td><td width="25%"><b>Location</b></td><td width="25%"><b>Owner</b></td><td><b>Size</b></td><td><b>Rent</b></td></tr>
-<?php 
-$HousesXML = simplexml_load_file($cfg['dirdata'].$cfg['house_file']);
-$MySQL = AAC::$SQL;
-$MySQL->myQuery('SELECT `players`.`name`, `houses`.`id` FROM `players`, `houses` WHERE `houses`.`owner` = `players`.`id`;');
-if ($MySQL->failed())
-	throw new aacException('SQL query failed:<br/>'.$SQL->getError());
-while ($row = $MySQL->fetch_array()){
-	$houses[(int)$row['id']] = $row['name'];
-}
-foreach ($HousesXML->house as $house){
-	$i++;
-	$list.= '<tr '.getStyle($i).'><td>'.htmlspecialchars($house['name']).'</td><td>'.htmlspecialchars($cfg['temple'][(int)$house['townid']]['name']).'</td><td>'.$houses[(int)$house['houseid']].'</td><td>'.$house['size'].'</td><td>'.$house['rent'].'</td></tr>'."\r\n";
-}
-echo $list;
-
-?>
-</table>
-</div>
-</div>
-<div class="bot"></div>
+    <div class="top">Houses</div>
+    <div class="mid">
+        <div style="height: 500px; overflow: auto; margin: 10px;">
+            <table>
+                <tr class="color0">
+                    <td width="35%"><b>House</b></td>
+                    <td width="25%"><b>Location</b></td>
+                    <td width="25%"><b>Owner</b></td>
+                    <td><b>Size</b></td>
+                    <td><b>Rent</b></td>
+                </tr>
+                <?php
+                $HousesXML = simplexml_load_file($cfg['dirdata'] . $cfg['house_file']);
+                $MySQL = AAC::$SQL;
+                $MySQL->myQuery('SELECT `players`.`name`, `houses`.`id` FROM `players`, `houses` WHERE `houses`.`owner` = `players`.`id`;');
+                if ($MySQL->failed())
+                    throw new aacException('SQL query failed:<br/>' . $SQL->getError());
+                $houses = [];
+                $i = 0;
+                while ($row = $MySQL->fetch_array()) {
+                    $houses[(int)$row['id']] = $row['name'];
+                }
+                $list = '';
+                foreach ($HousesXML->house as $house) {
+                    $i++;
+                    $list .= '<tr ' . getStyle($i) . '><td>' . htmlspecialchars($house['name']) . '</td><td>' . htmlspecialchars($cfg['temple'][(int)$house['townid']]['name']) . '</td><td>' . ($houses[(int)$house['houseid']] ?? '') . '</td><td>' . $house['size'] . '</td><td>' . $house['rent'] . '</td></tr>' . "\r\n";
+                }
+                echo $list;
+                ?>
+            </table>
+        </div>
+    </div>
+    <div class="bot"></div>
 </div>
 <?php include('footer.inc.php');?>
