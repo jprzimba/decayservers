556af8fdb3d5487436e612f1055ed0ee5781fb60
João Paulo <jprzimba@gmail.com>
2024-03-24 14:59:37 -0300

    * updated spells

--- 792/htdocs/Nicaw/spells.php
+++ 792/htdocs/Nicaw/spells.php
@@ -1,6 +1,6 @@
-<?
+<?php 
 /*
-    Copyright (C) 2007  Nicaw
+    Copyright (C) 2006  Nicaw
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -16,54 +16,93 @@
     with this program; if not, write to the Free Software Foundation, Inc.,
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
-include ("include.inc.php");
-$ptitle= "Spells - $cfg[server_name]";
-include ("header.inc.php");
+include('config.inc.php');
+include('include.inc.php');
+$ptitle = "Server Info - $cfg[server_name]";
+include("header.inc.php");
 ?>
 <div id="content">
 <div class="top">Spells</div>
 <div class="mid">
-<form method="get" action="spells.php"> 
+<form method="get" action="spells.php">
 <input type="text" name="spell"/> 
 <input type="submit" value="Search"/> 
 </form>
-<div style="height: 500px; overflow: auto; margin: 10px;">
-<?
-$SpellsXML = @simplexml_load_file($cfg['dirdata'].'spells/spells.xml');
-if ($SpellsXML === false) throw new aacException('spells.xml not found');
-foreach ($SpellsXML->instant as $spell){
-	if (!empty($_GET['spell']) and (stristr($spell['name'], $_GET['spell']) or stristr($spell['words'], $_GET['spell'])) or !isset($_GET['spell'])){
-		echo '<table><tr><td width="150px"><b>Spell Name</b></td><td>'.$spell['name'].'</td></tr>';
-		echo '<tr><td><b>Type</b></td><td>Instant</td></tr>';
-		echo '<tr><td><b>Words</b></td><td>'.$spell['words'].'</td></tr>';
-		if ($spell['lvl'] > 0)
-			echo '<tr><td><b>Required Level</b></td><td>'.$spell['lvl'].'</td></tr>';
-		if ($spell['maglv'] > 0)
-			echo '<tr><td><b>Magic Level</b></td><td>'.$spell['maglv'].'</td></tr>';
-		echo '<tr><td><b>Mana</b></td><td>'.$spell['mana'].'</td></tr>';
-		echo '<tr><td><b>Premium</b></td><td>'.(($spell['prem'] == 1)?'yes':'no').'</td></tr>';
-		echo '</table><hr/>';
-	}
+
+<?php
+$SpellsXML = simplexml_load_file($cfg['dirdata'] . 'spells/spells.xml');
+if ($SpellsXML === false) {
+    throw new aacException('spells.xml not found');
 }
-foreach ($SpellsXML->conjure as $spell){
-	if (!empty($_GET['spell']) and (stristr($spell['name'], $_GET['spell']) or stristr($spell['words'], $_GET['spell'])) or !isset($_GET['spell'])){
-		echo '<table><tr><td width="150px"><b>Spell Name</b></td><td>'.$spell['name'].'</td></tr>';
-		echo '<tr><td><b>Type</b></td><td>Conjure</td></tr>';
-		echo '<tr><td><b>Words</b></td><td>'.$spell['words'].'</td></tr>';
-		if ($spell['lvl'] > 0)
-			echo '<tr><td><b>Required Level</b></td><td>'.$spell['lvl'].'</td></tr>';
-		if ($spell['maglv'] > 0)
-			echo '<tr><td><b>Magic Level</b></td><td>'.$spell['maglv'].'</td></tr>';
-		echo '<tr><td><b>Mana</b></td><td>'.$spell['mana'].'</td></tr>';
-		echo '<tr><td><b>Premium</b></td><td>'.(($spell['prem'] == 1)?'yes':'no').'</td></tr>';
-		echo '<tr><td><b>Soul Points</b></td><td>'.$spell['soul'].'</td></tr>';
-		echo '<tr><td><b>Conjure Count</b></td><td>'.$spell['conjureCount'].'</td></tr>';
-		echo '</table><hr/>';
-	}
+
+foreach ($SpellsXML->instant as $spell) {
+    $spellName = (string) $spell['name'];
+    $spellWords = (string) $spell['words'];
+    $spellLvl = (int) $spell['lvl'];
+    $spellMagLvl = (int) $spell['maglv'];
+    $spellMana = (int) $spell['mana'];
+    $spellPrem = (int) $spell['prem'];
+    $vocations = array();
+
+    foreach ($spell->vocation as $vocation) {
+        $vocations[] = (string) $vocation['name'];
+    }
+
+    if (empty($_GET['spell']) || (stripos($spellName, $_GET['spell']) !== false) || (stripos($spellWords, $_GET['spell']) !== false)) {
+        echo '<table>';
+        echo '<tr><td width="150px"><b>Spell Name</b></td><td>' . htmlspecialchars($spellName) . '</td></tr>';
+        echo '<tr><td><b>Type</b></td><td>Instant</td></tr>';
+        echo '<tr><td><b>Words</b></td><td>' . htmlspecialchars($spellWords) . '</td></tr>';
+        if ($spellLvl > 0) {
+            echo '<tr><td><b>Required Level</b></td><td>' . $spellLvl . '</td></tr>';
+        }
+        if ($spellMagLvl > 0) {
+            echo '<tr><td><b>Magic Level</b></td><td>' . $spellMagLvl . '</td></tr>';
+        }
+        echo '<tr><td><b>Mana</b></td><td>' . $spellMana . '</td></tr>';
+        echo '<tr><td><b>Premium</b></td><td>' . (($spellPrem == 1) ? 'yes' : 'no') . '</td></tr>';
+        echo '<tr><td><b>Vocations</b></td><td>' . implode(', ', $vocations) . '</td></tr>';
+        echo '</table><hr/>';
+    }
+}
+
+foreach ($SpellsXML->conjure as $spell) {
+    $spellName = (string) $spell['name'];
+    $spellWords = (string) $spell['words'];
+    $spellLvl = (int) $spell['lvl'];
+    $spellMagLvl = (int) $spell['maglv'];
+    $spellMana = (int) $spell['mana'];
+    $spellPrem = (int) $spell['prem'];
+    $spellSoul = (int) $spell['soul'];
+    $spellConjureCount = (int) $spell['conjureCount'];
+    $vocations = array();
+
+    foreach ($spell->vocation as $vocation) {
+        $vocations[] = (string) $vocation['name'];
+    }
+
+    if (empty($_GET['spell']) || (stripos($spellName, $_GET['spell']) !== false) || (stripos($spellWords, $_GET['spell']) !== false)) {
+        echo '<table>';
+        echo '<tr><td width="150px"><b>Spell Name</b></td><td>' . htmlspecialchars($spellName) . '</td></tr>';
+        echo '<tr><td><b>Type</b></td><td>Conjure</td></tr>';
+        echo '<tr><td><b>Words</b></td><td>' . htmlspecialchars($spellWords) . '</td></tr>';
+        if ($spellLvl > 0) {
+            echo '<tr><td><b>Required Level</b></td><td>' . $spellLvl . '</td></tr>';
+        }
+        if ($spellMagLvl > 0) {
+            echo '<tr><td><b>Magic Level</b></td><td>' . $spellMagLvl . '</td></tr>';
+        }
+        echo '<tr><td><b>Mana</b></td><td>' . $spellMana . '</td></tr>';
+        echo '<tr><td><b>Premium</b></td><td>' . (($spellPrem == 1) ? 'yes' : 'no') . '</td></tr>';
+        echo '<tr><td><b>Vocations</b></td><td>' . implode(', ', $vocations) . '</td></tr>';
+        echo '<tr><td><b>Soul Points</b></td><td>' . $spellSoul . '</td></tr>';
+        echo '<tr><td><b>Conjure Count</b></td><td>' . $spellConjureCount . '</td></tr>';
+        echo '</table><hr/>';
+    }
 }
 ?>
-</div>
+
 </div>
 <div class="bot"></div>
 </div>
-<?include ("footer.inc.php");?>
+<?include ('footer.inc.php');?>
